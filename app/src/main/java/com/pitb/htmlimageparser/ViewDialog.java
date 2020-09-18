abc5bbeda332c99fd602175b53ee36567888d6a2
dev <montoo>
2020-09-18 12:13:43 +0530

    commit

---
+++ app/src/main/java/com/pitb/htmlimageparser/ViewDialog.java
@@ -0,0 +1,39 @@
+package com.pitb.htmlimageparser;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.Window;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class ViewDialog {
+    private Activity activity;
+    private Dialog dialog;
+
+    public ViewDialog(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void showDialog() {
+        dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.custom_loading_layout);
+
+        ImageView gifImageView = dialog.findViewById(R.id.custom_loading_imageView);
+
+        Glide.with(activity)
+                .load(R.drawable.loading)
+                .placeholder(R.drawable.loading)
+                .centerCrop()
+                .into(gifImageView);
+
+        dialog.show();
+    }
+
+    public void hideDialog() {
+        dialog.dismiss();
+    }
+}
